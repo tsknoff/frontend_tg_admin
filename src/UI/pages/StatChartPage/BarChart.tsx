987acedda0e804f36621b117d75e878f1cd95164
Egor Tsukanov <ts.eg.99@gmail.com>
2024-06-23 21:59:25 +0200

    bla

--- src/UI/pages/StatChartPage/BarChart.tsx
+++ src/UI/pages/StatChartPage/BarChart.tsx
@@ -20,18 +20,18 @@ ChartJS.register(
   Legend,
 );
 
-const data = {
-  labels: ["January", "February", "March", "April", "May", "June", "July"],
-  datasets: [
-    {
-      label: "Dataset 1",
-      data: [65, 59, 80, 81, 56, 55, 40],
-      backgroundColor: "rgba(75, 192, 192, 0.2)",
-      borderColor: "rgba(75, 192, 192, 1)",
-      borderWidth: 1,
-    },
-  ],
-};
+// const data = {
+//   labels: ["January", "February", "March", "April", "May", "June", "July"],
+//   datasets: [
+//     {
+//       label: "Dataset 1",
+//       data: [65, 59, 80, 81, 56, 55, 40],
+//       backgroundColor: "rgba(75, 192, 192, 0.2)",
+//       borderColor: "rgba(75, 192, 192, 1)",
+//       borderWidth: 1,
+//     },
+//   ],
+// };
 
 const options = {
   responsive: true,
