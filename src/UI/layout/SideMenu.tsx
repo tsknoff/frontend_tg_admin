afd87adf7ef9aacd9e3b7124d68a422d95192922
Egor Tsukanov <ts.eg.99@gmail.com>
2024-06-26 17:39:39 +0200

    1) Redirect to login if there is no token                 2) Updating texts after saving                 3) Hid the mailings                 4) Added logout                 5) Made in groups with the author 0 save disabled                 6) Username in the corner of the screen

--- src/UI/layout/SideMenu.tsx
+++ src/UI/layout/SideMenu.tsx
@@ -7,7 +7,7 @@ import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 import DashboardIcon from "@mui/icons-material/Dashboard";
-import Email from "@mui/icons-material/Email";
+// import Email from "@mui/icons-material/Email";
 import Group from "@mui/icons-material/Group";
 import BarChart from "@mui/icons-material/BarChart";
 import { Link, useLocation } from "react-router-dom";
