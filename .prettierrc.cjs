0f664ea49fb556339708e6fbd21f725af9bc7d26
Egor Tsukanov <ts.eg.99@gmail.com>
2024-06-26 16:44:13 +0200

    try 2

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,13 +1,13 @@
 module.exports = {
-  "useTabs": false,
-  "printWidth": 80,
-  "semi": true,
-  "singleQuote": false,
-  "jsxSingleQuote": false,
-  "tabWidth": 2,
-  "trailingComma": "all",
-  "bracketSpacing": true,
-  "jsxBracketSameLine": false,
-  "arrowParens": "always",
-  "endOfLine": "auto"
-}
+  useTabs: false,
+  printWidth: 80,
+  semi: true,
+  singleQuote: false,
+  jsxSingleQuote: false,
+  tabWidth: 2,
+  trailingComma: "all",
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: "always",
+  endOfLine: "auto",
+};
